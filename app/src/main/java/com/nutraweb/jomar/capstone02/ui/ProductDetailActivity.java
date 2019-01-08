f0aa2b192d6ee12d1cb33b18da173cffa4a2099e
JoaoSantosBH <joaomarcelo.ms@gmail.com>
2019-01-08 20:08:29 -0200

    precificacaoStock

--- app/src/main/java/com/nutraweb/jomar/capstone02/ui/ProductDetailActivity.java
+++ app/src/main/java/com/nutraweb/jomar/capstone02/ui/ProductDetailActivity.java
@@ -15,6 +15,8 @@ import com.nutraweb.jomar.capstone02.R;
 import com.nutraweb.jomar.capstone02.model.ProductEntity;
 import com.squareup.picasso.Picasso;
 
+import java.text.NumberFormat;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -22,9 +24,19 @@ public class ProductDetailActivity extends AppCompatActivity {
 
     @BindView(R.id.title_Prod_textView)
     TextView title;
-
+    @BindView(R.id.product_price_textView)
+    TextView price;
     @BindView(R.id.id_imageView)
     ImageView thumb;
+    @BindView(R.id.total_products_value_textView)
+    TextView total;
+    @BindView(R.id.quantity_text_view)
+    TextView qty;
+
+
+    private int mQuantity = 0;
+    private int mTotalPrice = 0 ;
+    private int value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +49,12 @@ public class ProductDetailActivity extends AppCompatActivity {
         this.setTitle(R.string.title_Add_Item_Stock);
 
         if (getIntent().hasExtra("myObj")) {
-
             product = (ProductEntity) getIntent().getParcelableExtra("myObj");
+            value = product.getValor();
             title.setText(product.getTitulo());
+            price.setText("R$ " + String.valueOf(product.getValor()) +" ,00" );
+            total.setText(String.valueOf(mTotalPrice));
+            qty.setText(String.valueOf(mQuantity));
             Context context = this;
             Picasso.with(context)
                     .load(Uri.parse(product.getUrl()))
@@ -54,6 +69,38 @@ public class ProductDetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void increment(View view) {
+
+        mQuantity = mQuantity + 1;
+        displayQuantity(mQuantity);
+        mTotalPrice = calculatePrice();
+        displayCost(mTotalPrice);
+    }
+    public void decrement(View view) {
+        if (mQuantity > 0) {
+
+            mQuantity = mQuantity - 1;
+            displayQuantity(mQuantity);
+            mTotalPrice = calculatePrice();
+            displayCost(mTotalPrice);
+        }
+    }
+    private void displayQuantity(int numberOfProducts) {
+        qty.setText(String.valueOf(numberOfProducts));
+    }
+    private void displayCost(int totalPrice) {
+
+        String convertPrice = NumberFormat.getCurrencyInstance().format(totalPrice);
+        total.setText(convertPrice);
+    }
+    private int calculatePrice() {
+
+                mTotalPrice = mQuantity * value;
+
+
+        return mTotalPrice;
     }
 
 }
